2d65ef6b9228465f5a980b0194d520f51361b9b2
Satyajit Barik <satyajitbarik83@gmail.com>
2023-07-10 17:26:24 +0530

    beowser resp

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 // const POST = process.env.PORT | 8080
 const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Azure!'); // This sends the response "Hello, world!" to the browser
+});
 
 app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
